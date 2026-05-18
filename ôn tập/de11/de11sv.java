fb835977472978083b009ff616884cd45fa7fb41
Bùi Minh Đức <tommyminecraft2005@gmail.com>
2026-05-18 15:39:53 +0000

    gkg

--- ôn tập/de11/de11sv.java
+++ ôn tập/de11/de11sv.java
@@ -8,7 +8,7 @@ public class de11sv {
         ServerSocket ss = new ServerSocket(5000);
         System.out.println("Server dang ket noi toi client!");
         Socket s = ss.accept();
-        System.out.println("Server da ket noo toi client");
+        System.out.println("Server da ket noi toi client");
         DataInputStream in = new DataInputStream(s.getInputStream());
         int n = in.readInt();
         if(n<=0) {
@@ -17,14 +17,17 @@ public class de11sv {
         }
         else{
             List<Double> a = new ArrayList<>();
+            System.out.print("Mang da nhap: ");
             for(int i=0;i<n;i++){
                 Double t = in.readDouble();
                 a.add(t);
+                System.out.print(" " + t);
             }
             Double sum = 0.0;
             for(Double i : a){
                 sum+=i;
             }
+            System.out.println();
             System.out.println("TBC = "+sum/n);
         }
         in.close();
